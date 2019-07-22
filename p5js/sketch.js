fffd6c75369120fa4db3cff09fa1f99aec283613
Unai <unai.san.rod@techtalents.club>
2019-07-22 13:58:08 +0200

    Add files via upload

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,6 +1,6 @@
 
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(1200, 400);
   }
   let timer = 0
 function draw(){
@@ -9,7 +9,7 @@ function draw(){
         //drawBar('5d30391ac9af6a001746cdf7','#0fffff')
         drawGraph('5d30391ac9af6a001746cdf7','#0fffff')
         //drawBar('5d304d300c36f30017cfe944','#faab99')
-        drawGraph('5d304d300c36f30017cfe944','#faab99')
+        //drawGraph('5d304d300c36f30017cfe944','#faab99')
         
     }
 }
@@ -27,11 +27,11 @@ function drawBar(id,color){
         strokeWeight(1)
         data.forEach(function(element,index){
             //ellipse((600/data.length)*index,400-element.value,1,1)
-            if(x<=600){
+            if(x<=1200){
             y= 400-element.value
             x=(600/data.length)*(index)
             
-            rect(x-(600/data.length)/2,y,(600/data.length),400-y);
+            rect(x-(1200/data.length)/2,y,(1200/data.length),400-y);
             
             }
         })
@@ -49,13 +49,13 @@ function drawGraph(id,color){
     .then(function(data){
         //console.log(data)
         stroke(color)
-        strokeWeight(4)
+        strokeWeight(1)
         data.forEach(function(element,index){
             //ellipse((600/data.length)*index,400-element.value,1,1)
-            if(x2<=600){
+            if(x2<=1200){
             y2= 400-element.value
-            x1=(600/data.length)*(index-1)
-            x2=(600/data.length)*(index)
+            x1=(1200/data.length)*(index-1)
+            x2=(1200/data.length)*(index)
             line(x1,y1,x2,y2);
             y1=y2
             }
